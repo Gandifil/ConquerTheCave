114e0022d38ecd4463f36f4c7d85ec1b15296bf7
Gandifil <gandifilmk@gmail.com>
2024-01-15 20:16:38 +0300

    Add mods adding and event event for it.

--- ConquerTheDungeon/Logic/CreatureCard.cs
+++ ConquerTheDungeon/Logic/CreatureCard.cs
@@ -1,16 +1,26 @@
+using System;
 using System.Collections.Generic;
 
 namespace ConquerTheDungeon.Logic;
 
 public class CreatureCard : Card
 {
+    private readonly List<ModCard> _mods = new();
     public int Life => Content.Life;
 
     public int Damage => Content.Damage;
 
-    public List<ModCard> Mods { get; set; }
+    public event EventHandler<ModCard> OnModAdded;
+
+    public IReadOnlyList<ModCard> Mods => _mods;
     
     public CreatureCard(CardContent content) : base(content)
     {
+    }
+
+    public void Add(ModCard mod)
+    {
+        _mods.Add(mod);
+        OnModAdded?.Invoke(this, mod);
     }
 }
