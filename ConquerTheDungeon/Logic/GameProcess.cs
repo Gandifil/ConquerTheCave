1c57ab5975d1c70febc536c998c07ed2e9416255
Gandifil <gandifilmk@gmail.com>
2024-02-01 00:24:45 +0300

    Add special logic for using cards.

--- ConquerTheDungeon/Logic/GameProcess.cs
+++ ConquerTheDungeon/Logic/GameProcess.cs
@@ -1,26 +1,41 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ConquerTheDungeon.Logic.Cards;
 using MLEM.Extensions;
+using MonoGame.Extended.Collections;
 
 namespace ConquerTheDungeon.Logic;
 
 public class GameProcess
 {
     private readonly Player _player;
-    public readonly Board PlayerBoard = new();
+    private readonly List<Card> _backCards;
+    private readonly ObservableCollection<Card> _currentCards;
     
+    public readonly Board PlayerBoard = new();
     public readonly Board EnemyBoard = new();
+    public IObservableCollection<Card> CurrentCardsEvents => _currentCards;
+    public IReadOnlyCollection<Card> CurrentCards => _currentCards;
 
     public GameProcess(Player player)
     {
         _player = player;
+        _backCards = player.Cards.ToList();
+        _currentCards = new(_backCards);
     }
 
     public void Initialization()
     {
         foreach (var card in new FightScenario().GetInitialCards())
             EnemyBoard.Creatures.Add(card.Clone());
+    }
+
+    public void Use(Card card)
+    {
+        foreach (var item in _currentCards.ToList())
+            if (item.GetType().IsAssignableTo(card.GetType()))
+                _currentCards.Remove(item);
     }
 
     public void Turn()
@@ -48,5 +63,14 @@ public class GameProcess
                 _dirtyCards.Add(card);
                 return;
             }
+
+        RefreshCurrentCards();
+    }
+
+    private void RefreshCurrentCards()
+    {
+        foreach (var card in _backCards)
+            if (!_currentCards.Contains(card))
+                _currentCards.Add(card);
     }
 }
